e5771953e6b461cd4642b57c169ab14802087421
AlexMinez <alex_minihan@outlook.com>
2022-03-27 14:59:07 +1100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+
+
+
+function findMatching(drivers, matching){
+  return drivers.filter( possibleMatch => possibleMatch.toLowerCase() === matching.toLowerCase()
+  )
+}
+
+function fuzzyMatch(drivers,matching){
+    return drivers.filter(possibleMatch =>
+        possibleMatch.toLowerCase().indexOf(matching.toLowerCase()) === 0
+    )
+}
+function matchName(drivers, matching){
+    return drivers.filter(record => record.name === matching)
+}
